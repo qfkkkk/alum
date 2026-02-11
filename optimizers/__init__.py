dd3f0a8f8dd015af01c3bcd79910df197b7e04c0
wangzhuoyang <wangzhuoyang@ciictec.com>
2026-02-11 19:40:47 +0800

    chore:删除注释

--- optimizers/__init__.py
+++ optimizers/__init__.py
@@ -1,12 +1,3 @@
-'''
-Author: wangzhuoyang wangzhuoyang@ciictec.com
-Date: 2026-02-11 16:20:23
-LastEditors: wangzhuoyang wangzhuoyang@ciictec.com
-LastEditTime: 2026-02-11 17:56:38
-FilePath: /alum/optimizers/__init__.py
-Description: 这是默认设置,请设置`customMade`, 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
-'''
-
 from .base_optimizer import BaseOptimizer
 from .dummy_optimizer import DummyOptimizer
 
